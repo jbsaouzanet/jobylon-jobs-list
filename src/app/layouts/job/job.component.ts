649a00753af220cc3bd00da60c3837190d1199a4
JBS <jbsaouzanet@gmail.com>
2020-12-05 14:31:54 +0100

    0005 - Fix Responsive

--- src/app/layouts/job/job.component.ts
+++ src/app/layouts/job/job.component.ts
@@ -4,6 +4,7 @@ import { JobListService } from '../../shared/jobs-list/jobs-list.service';
 import { Jobs } from '../../model/jobs.model';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogContent } from '../dialog-content/dialog-content.component';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
     selector: 'job',
@@ -14,21 +15,30 @@ import { DialogContent } from '../dialog-content/dialog-content.component';
 export class JobComponent  implements  OnInit {
 
     title = 'jobylon-job';
+    id: string
     url: string;
     jobs: Jobs; 
-    constructor (private jobListService:JobListService, public dialog: MatDialog) {}
+    jobList: Array<Jobs>; 
+    data: any
+    constructor (private jobListService:JobListService, public dialog: MatDialog, private route: ActivatedRoute) {}
   
     ngOnInit() {
-         this.url = "https://feed.jobylon.com/feeds/7d7e6fd12c614aa5af3624b06f7a74b8/";
-         
+        this.id = this.route.snapshot.paramMap.get('id')
+        this.url = "https://feed.jobylon.com/feeds/7d7e6fd12c614aa5af3624b06f7a74b8/";
+        this.getJobsList();
+       
+       
     }
 
     getJobsList() {
         console.log(this.url);
             this.jobListService.get(this.url)
             .subscribe(response => {
-            this.jobs = response;
-            console.log(this.jobs);
+            this.jobList = new Array(response);
+            //console.log(this.jobList);  
+            //console.log(Number(this.id));
+            //this.data = this.jobList.find(i => i.id === Number(this.id));   
+            //console.log(this.jobList.find(i => i.id === Number(this.id)));       
             });
     }    
 }
